fdd0685336a74f22b3c961d9afb33397d04c93d2
dsmelser <dsmelser.personal@gmail.com>
2020-10-11 22:50:52 -0500

    initial commit of spring boot skeleton

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//this is a comment
+	//I'm making a note here
+	//huge success
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
